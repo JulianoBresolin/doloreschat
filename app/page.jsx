9ec9aa431bbb6dc6fe8f7dc7b654d1dba0267858
juliano bresolin <bresolinfotografia@gmail.com>
2023-08-22 00:53:05 -0300

    22-a

--- app/page.jsx
+++ app/page.jsx
@@ -36,6 +36,10 @@ export default function Chat() {
 					Dolores AI
 				</h1>
 				<div className="border-2 border-slate-500 h-[80%] bg-slate-600 overflow-auto p-5 rounded-xl">
+				{messages.length === 0 ? (
+        <p className="text-lg text-slate-400">Faça uma pergunta para começar , você pode selecionar alguma das questões  abaixo! </p>
+      ) : (
+		<div>
 					{messages.map((m) => (
 						<div className=" flex gap-2 py-2 " key={m.id}>
 							{m.role === "user" ? (
@@ -48,6 +52,8 @@ export default function Chat() {
 							{m.content}
 						</div>
 					))}
+					</div>
+					)}
 				</div>
 
 				<form
